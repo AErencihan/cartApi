26c52fedef6e7846c69b923316754340dd248c30
AErencihan <eren.cihann@hotmail.com>
2024-03-21 21:51:26 +0300

    added and changed

--- src/main/java/com/example/cartapi/controller/CartController.java
+++ src/main/java/com/example/cartapi/controller/CartController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/cart")
+@RequestMapping("/cart/api/cart")
 @RequiredArgsConstructor
 public class CartController {
 
